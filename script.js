b062fa205c8372b896f9d44677161760a1c75b77
Kahraqueli <160247426+Kahraqueli@users.noreply.github.com>
2025-06-26 14:51:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ const sugestoes = {
         "Miyagi Sushi - Restaurante japonês com rodízio, buffet e opções à la carte"
     ],
     cafe: [
-        "Boccata Caffè e Cucina – Clássico com ótimos croissants e pratos leves,",
+        "Boccata Caffè e Cucina – Clássico com ótimos croissants e pratos leves",
         "Coffee O’Clock – Estilo industrial, dois andares, serve cafés e drinks",
         "Café du Coin - Café especial, torrado e preparado no local, com ambiente acolhedor e intimista."
     ],
     festa: [
         "Arena Pub - Boa estrutura para curtir, dançar e socializar com conforto. Combina festa animada com toques de requinte.",
         "Head Hunter - Atmosfera intimista com visual contemporâneo e cardápio sofisticado. Boa escolha para quem prefere algo mais reservado, mas ainda chique.",
-        "Boni Villa Steak House - 
-        Complexo gastronômico sofisticado com carnes nobres, sushi bar e wine cave.
+        `Boni Villa Steak House - Complexo gastronômico sofisticado com carnes nobres, sushi bar e wine cave.
 Ambiente elegante, ideal para aniversários, confraternizações ou festas exclusivas.
-Possui música ao vivo em alguns dias e excelente carta de vinhos."
+Possui música ao vivo em alguns dias e excelente carta de vinhos.`
     ]
 };
 
